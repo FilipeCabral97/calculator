b5afbcb7a60c8eb03075085a6107ff76c28b8685
Filipe Cabral <rui.filipe.cabral.97@gmail.com>
2022-09-21 11:04:54 +0100

    Create basic UI and operations

---
+++ script.js
@@ -0,0 +1,23 @@
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const operate = (op, a, b) => {
+  switch (op) {
+    case "+":
+      sum(a, b);
+      break;
+    case "-":
+      subtract(a, b);
+      break;
+    case "*":
+      multiply(a, b);
+      break;
+    case "/":
+      divide(a, b);
+      break;
+    default:
+      alert("Error");
+  }
+};
